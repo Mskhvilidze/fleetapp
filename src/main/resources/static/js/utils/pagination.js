9a0663ed2a90518c1e9374f7570ecd03552e100c
Mskhvilidze <33086639+Mskhvilidze@users.noreply.github.com>
2023-03-27 15:52:15 +0200

    pagination.js wurde verfeinert und zum Laufen gebracht

--- src/main/resources/static/js/utils/pagination.js
+++ src/main/resources/static/js/utils/pagination.js
@@ -1,18 +1,32 @@
-$('document').ready(function(){
-    //select Topics
+$(document).ready(function(){
     let tbody = document.querySelector("#tbody");
     let tr = tbody.getElementsByTagName("tr");
     let arrayTr = [];
     for (let i = 0; i < tr.length; i++) {
         arrayTr.push(tr[i]);
     }
+    pagination(tbody, tr, arrayTr);
+
+    $("#selectTopic").change(function () {
+        let neil = document.getElementsByClassName("page-item");
+        Array.from(neil).forEach((el) => el.remove());
+        pagination(tbody, tr, arrayTr);
+    });
+});
+
+function pagination(tbody, tr, arrayTr) {
+    //select Topics
     let liPage;
     let aPage;
     let nextPage = 1;
-    let value = 1;
-    if(arrayTr.length >= 2){
-        value = test();
-        alert(value);
+    let value = getSelectedValue();
+    let pageSize = parseInt(value);
+    // entferne alte Pagination-Elemente
+    //$(".pagination .page-item, .pagination .prev, .pagination .next").remove();
+    Array.from(document.getElementsByClassName("page-item")).forEach((el) =>
+        el.remove()
+    );
+    if (arrayTr.length >= 2) {
         let neil = document.getElementsByClassName("page-item");
         for (let i = 0; i < neil.length; i++) {
             neil[i].remove();
@@ -20,13 +34,13 @@ $('document').ready(function(){
         for (let i = 0; i < arrayTr.length; i++) {
             tbody.appendChild(arrayTr[i]);
         }
-        if (arrayTr.length >= 2) {
-            const nofPage = Math.ceil(arrayTr.length / value);
+        if (arrayTr.length >= pageSize) {
+            const nofPage = Math.ceil(arrayTr.length / pageSize);
             //pagination nach links, daher nextPage --
             prev().onclick = ev => {
                 if (nextPage > 1){
                     nextPage--;
-                    paginateTable(nextPage, value, arrayTr, tbody);
+                    paginateTable(nextPage, pageSize, arrayTr, tbody);
                 }
             }
             //Pagination wird erstellt
@@ -43,25 +57,25 @@ $('document').ready(function(){
                     let $target = $(ev.target);
                     let x = $target.attr("data-page");
                     nextPage = x;
-                    paginateTable(x, value, arrayTr, tbody);
+                    paginateTable(x, pageSize, arrayTr, tbody);
                 }
             }
             next().onclick = ev => {
                 if (nextPage < nofPage){
                     nextPage++;
-                    paginateTable(nextPage, value, arrayTr, tbody);
+                    paginateTable(nextPage, pageSize, arrayTr, tbody);
                 }
             }
-            paginateTable(1, value, arrayTr, tbody);
+            paginateTable(1, pageSize, arrayTr, tbody);
         }
     }
-});
+}
 
 function paginateTable(x, value, arrayTr, tbody){
+    nextPage = x;
     tbody.innerHTML = "";
-    x--;
-    let start = value * x;
-    let end = start + parseInt(value);
+    let start = (x - 1) * value;
+    let end = Math.min(start + value, arrayTr.length);
     let page = arrayTr.slice(start, end);
     for (let j = 0; j < page.length; j++) {
         tbody.appendChild(page[j]);
@@ -69,25 +83,26 @@ function paginateTable(x, value, arrayTr, tbody){
 }
 
 function prev(){
-            let prevPage = document.createElement("li");
-            let aPrev = document.createElement("a");
-            $(".pagination").append(prevPage);
-            aPrev.innerHTML = "&#139";
-            prevPage.appendChild(aPrev);
-            return aPrev;
+    let prevPage = document.createElement("li");
+    prevPage.className = "page-item";
+    let aPrev = document.createElement("a");
+    aPrev.innerHTML = "&#139";
+    prevPage.appendChild(aPrev);
+    $(".pagination").append(prevPage);
+    return aPrev;
 }
 
 function next(){
-           let prevPage = document.createElement("li");
-           let aNext = document.createElement("a");
-           $(".pagination").append(prevPage);
-           aNext.innerHTML = "&#155";
-           prevPage.appendChild(aNext);
-           return aNext;
+    let aNextPage = document.createElement("li");
+    let aNext = document.createElement("a");
+    aNextPage.className = "page-item";
+    aNext.innerHTML = "&#155";
+    aNextPage.appendChild(aNext);
+    $(".pagination").append(aNextPage);
+    return aNext;
 }
 
-
-function test(){
-            let value = $("#selectTopic option:selected").val();
-            return value;
+function getSelectedValue(){
+    let value = $("#selectTopic option:selected").val();
+    return value;
 }
